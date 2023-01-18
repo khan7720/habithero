eecfd30fb173b8411f7f3318fe37716b5c83c689
start.vaadin.com <no-reply@vaadin.com>
2023-01-18 11:30:55 +0000

    Generated project

---
+++ src/main/java/com/hankshop/habithero/data/service/SamplePersonRepository.java
@@ -0,0 +1,12 @@
+package com.hankshop.habithero.data.service;
+
+import com.hankshop.habithero.data.entity.SamplePerson;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface SamplePersonRepository
+        extends
+            JpaRepository<SamplePerson, Long>,
+            JpaSpecificationExecutor<SamplePerson> {
+
+}
---
+++ src/main/java/com/hankshop/habithero/views/stats/StatsView.java
@@ -0,0 +1,33 @@
+package com.hankshop.habithero.views.stats;
+
+import com.hankshop.habithero.views.MainLayout;
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+@PageTitle("Stats")
+@Route(value = "stats", layout = MainLayout.class)
+public class StatsView extends HorizontalLayout {
+
+    private TextField name;
+    private Button sayHello;
+
+    public StatsView() {
+        name = new TextField("Your name");
+        sayHello = new Button("Say hello");
+        sayHello.addClickListener(e -> {
+            Notification.show("Hello " + name.getValue());
+        });
+        sayHello.addClickShortcut(Key.ENTER);
+
+        setMargin(true);
+        setVerticalComponentAlignment(Alignment.END, name, sayHello);
+
+        add(name, sayHello);
+    }
+
+}
